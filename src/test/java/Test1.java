965622ad74b4f8d308161074ca9713e0aed2854d
polgelabert <polgelabert@gmail.com>
2017-10-16 16:13:34 +0200

    add test folder

---
+++ src/test/java/Test1.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class Test1 {
+
+
+
+
+    /*public static void main(String[] args)
+    {
+            System.out.println("Hola");
+    }*/
+
+
+/*
+    @Test
+    public void testCuaBuida() {
+        Cola c  = new ColaImpl<Integer>(10);
+
+        // assert statements
+        assertEquals("10 x 0 must be 0", 0, c.size(), 0);
+    }
+*/
+
+}
