b58b8cbd1ecd83e18416e7d066b66007c555bebb
gazorpAnna <anna.gmoreno95@gmail.com>
2017-10-16 12:37:20 +0200

    ans 2

--- src/C2.java
+++ src/C2.java
@@ -1,2 +1,10 @@
-public class C2 {
+import java.util.logging.Logger;
+
+public class C2 implements Command{
+
+    Logger log = Logger.getLogger("a");
+
+    public void execute(){
+        log.info("Classe C2 executada");
+    }
 }
--- src/C3.java
+++ src/C3.java
@@ -1,2 +1,11 @@
-public class C3 {
+import java.util.logging.Logger;
+
+public class C3 implements Command{
+
+    Logger log = Logger.getLogger("a");
+
+    public void execute(){
+        log.info("Classe C3 executada");
+    }
+
 }
